20ee7ac36853470d589473fe167343bbaa9424c1
Alexander Leffringhausen <alexander.leffringhausen@web.de>
2023-03-09 18:14:13 +0100

    Create BLEmodule.h

---
+++ Code/header/BLEmodule.h
@@ -0,0 +1,7 @@
+/*
+
+https://docs.arduino.cc/tutorials/nano-33-ble-sense/ble-device-to-device
+https://www.arduino.cc/reference/en/libraries/arduinoble/
+https://www.az-delivery.de/collections/esp32/products/esp32-dev-kit-c-unverlotet
+
+*/
